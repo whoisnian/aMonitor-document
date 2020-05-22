40d60f983300a16b08f2f7fd6d3f371fc0151164
whoisnian <zhuchangbao1998@gmail.com>
2020-05-22 17:23:41 +0800

    docs: 需求分析章节所用图表

---
+++ pic/svg/sed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#########################################################################
+# File Name: sed.sh
+# Author: nian
+# Blog: https://whoisnian.com
+# Mail: zhuchangbao1998@gmail.com
+# Created Time: 2020年05月21日 星期四 22时22分19秒
+#########################################################################
+sed -i 's/<text text-anchor="middle" font-size="10px" x="50%" y="100%">Viewer does not support full SVG 1.1<\/text>//gm' *.svg
